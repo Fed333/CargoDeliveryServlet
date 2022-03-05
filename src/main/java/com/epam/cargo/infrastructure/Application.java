df98928e4bf94cfddf698fd0af36b0d012494257
Fed333 <kovalchuk.roman03@gmail.com>
2022-03-06 00:39:02 +0200

    extracted code of initialization no lazy singletons in separated method

--- src/main/java/com/epam/cargo/infrastructure/Application.java
+++ src/main/java/com/epam/cargo/infrastructure/Application.java
@@ -1,12 +1,12 @@
-package com.epam.cargo.infrastructure.application;
+package com.epam.cargo.infrastructure;
 
-import com.epam.cargo.infrastructure.ApplicationContext;
+import com.epam.cargo.infrastructure.annotation.CommandMapping;
+import com.epam.cargo.infrastructure.context.ApplicationContext;
 import com.epam.cargo.infrastructure.annotation.Singleton;
 import com.epam.cargo.infrastructure.config.Config;
 import com.epam.cargo.infrastructure.config.impl.JavaConfig;
 import com.epam.cargo.infrastructure.factory.ObjectFactory;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @SuppressWarnings("rawtypes")
@@ -17,12 +17,16 @@ public class Application {
         ObjectFactory factory = new ObjectFactory(context);
         context.setFactory(factory);
 
+        initNoLazySingletons(config, context);
+
+        return context;
+    }
+
+    private static void initNoLazySingletons(Config config, ApplicationContext context) {
         for (Class<?> clazz : config.getScanner().getTypesAnnotatedWith(Singleton.class)) {
             if (clazz.getAnnotation(Singleton.class).type().equals(Singleton.Type.EAGER)){
                 context.getObject(clazz);
             }
         }
-
-        return context;
     }
 }
