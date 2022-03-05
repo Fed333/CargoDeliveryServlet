4c95f051b102460205996c6e81c337238799548e
Fed333 <kovalchuk.roman03@gmail.com>
2022-03-06 00:32:00 +0200

    added new annotations to the infrastructure

---
+++ src/main/java/com/epam/cargo/infrastructure/annotation/CommandMapping.java
@@ -0,0 +1,12 @@
+package com.epam.cargo.infrastructure.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CommandMapping {
+
+    String mapping();
+}
