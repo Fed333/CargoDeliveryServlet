d1fd33b2bda8417c56bdad9b34fa94dc1990daa9
Fed333 <kovalchuk.roman03@gmail.com>
2022-03-06 18:22:49 +0200

    added some business logic to CityService

--- src/main/java/com/epam/cargo/service/CityService.java
+++ src/main/java/com/epam/cargo/service/CityService.java
@@ -3,6 +3,7 @@ package com.epam.cargo.service;
 import com.epam.cargo.dao.repo.CityRepo;
 import com.epam.cargo.entity.City;
 import com.epam.cargo.infrastructure.annotation.Inject;
+import com.epam.cargo.infrastructure.annotation.PropertyValue;
 import com.epam.cargo.infrastructure.annotation.Singleton;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Singleton
 public class CityService {
+
+    @PropertyValue(property = "messages")
+    private String messages;
 
     @Inject
     private CityRepo cityRepo;
@@ -21,9 +25,18 @@ public class CityService {
         return cityRepo.findAll();
     }
 
-    @PostConstruct
-    public void init(){
-        System.out.println("CityService: [init]. CityRepo: " + cityRepo);
+    /**
+     * add City object to database if city with such zipcode is absent
+     * @param city city for adding
+     * @return true if object was successfully added, otherwise false
+     * */
+    public boolean addCity(City city){
+        if (cityRepo.findByZipcode(city.getZipcode()).isPresent()){
+            return false;
+        }
+        cityRepo.save(city);
 
+        return true;
     }
+
 }
