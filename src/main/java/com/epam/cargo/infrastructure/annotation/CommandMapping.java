70989c9656f398e1ecbce61c4435e518a6a9efb2
Fed333 <kovalchuk.roman03@gmail.com>
2022-03-06 18:18:12 +0200

    marked @CommandMapping annotation as deprecate removed usage this method of request mapping in servlet application

--- src/main/java/com/epam/cargo/infrastructure/annotation/CommandMapping.java
+++ src/main/java/com/epam/cargo/infrastructure/annotation/CommandMapping.java
@@ -1,12 +1,15 @@
 package com.epam.cargo.infrastructure.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Inherited;
+import com.epam.cargo.infrastructure.dispatcher.HttpMethod;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Deprecated
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CommandMapping {
 
     String mapping();
+
+    HttpMethod method();
 }
