0ba7526728eabb3e60a10150e12993dd9ec24b03
Fed333 <kovalchuk.roman03@gmail.com>
2022-03-07 21:31:44 +0200

    added controller for directions page

--- src/main/java/com/epam/cargo/controller/DirectionsController.java
+++ src/main/java/com/epam/cargo/controller/DirectionsController.java
@@ -1,14 +1,22 @@
 package com.epam.cargo.controller;
 
+import com.epam.cargo.entity.DirectionDelivery;
 import com.epam.cargo.infrastructure.annotation.Controller;
+import com.epam.cargo.infrastructure.annotation.Inject;
 import com.epam.cargo.infrastructure.annotation.RequestAttribute;
 import com.epam.cargo.infrastructure.annotation.RequestMapping;
 import com.epam.cargo.infrastructure.dispatcher.HttpMethod;
 import com.epam.cargo.infrastructure.web.Model;
+import com.epam.cargo.service.DirectionDeliveryService;
+
+import java.util.List;
 
 @SuppressWarnings("unused")
 @Controller
 public class DirectionsController {
+
+    @Inject
+    private DirectionDeliveryService directionsService;
 
     @RequestMapping(url = "/directions", method = HttpMethod.GET)
     public String directions(
@@ -16,7 +24,8 @@ public class DirectionsController {
             @RequestAttribute(name = "response", defaultValue = "default Response")
             String response
     ){
-
+        List<DirectionDelivery> directions = directionsService.findAll();
+        model.addAttribute("directions", directions);
         model.addAttribute("response", response);
         return "directions.jsp";
     }
