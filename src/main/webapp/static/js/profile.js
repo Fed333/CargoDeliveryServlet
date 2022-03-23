608636addc19566d9492f64273fe65360d112235
Fed333 <kovalchuk.roman03@gmail.com>
2022-03-23 17:07:15 +0200

    removed redundant console log

--- src/main/webapp/static/js/profile.js
+++ src/main/webapp/static/js/profile.js
@@ -3,12 +3,10 @@ function setChangingMenuItemsHandlers(profileMenuItemsId){
     let childNodes = document.getElementById(profileMenuItemsId).childNodes;
 
     for(let child of childNodes){
-        console.log(child)
         child.addEventListener('click', ()=>{
             //найде вибраний елемент
             let el = document.querySelector(`ul[id=${profileMenuItemsId}] li.nav-item button.nav-link.active`)
             activePillHiddenInput.setAttribute('value', el.id)
-            console.log('id:' + el.id)
         })
     }
 }
