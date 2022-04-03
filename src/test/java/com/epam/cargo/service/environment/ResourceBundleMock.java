654c60ffaa0fa8c079e33f4f35826fdc37c768c9
Fed333 <kovalchuk.roman03@gmail.com>
2022-04-04 00:58:51 +0300

    added test environment classes

---
+++ src/test/java/com/epam/cargo/service/environment/ResourceBundleMock.java
@@ -0,0 +1,33 @@
+package com.epam.cargo.service.environment;
+
+import org.mockito.MockedStatic;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
+
+public class ResourceBundleMock {
+
+    private MockedStatic<ResourceBundle> bundleMockedStatic;
+
+    public void mockResourceBundle() throws IOException {
+
+        bundleMockedStatic = mockStatic(ResourceBundle.class);
+        bundleMockedStatic
+                .when(()->ResourceBundle.getBundle(any()))
+                .thenAnswer(invocationOnMock -> {
+                    String baseName = invocationOnMock.getArgument(0, String.class);
+                    FileInputStream fis = new FileInputStream("src/main/webapp/WEB-INF/classes" + baseName + ".properties");
+                    return new PropertyResourceBundle(fis);
+                });
+    }
+
+    public void closeMock(){
+        bundleMockedStatic.close();
+    }
+
+}
