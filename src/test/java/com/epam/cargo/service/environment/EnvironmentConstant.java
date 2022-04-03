f2fc969ec0572685cf70917c6923482374cff233
Fed333 <kovalchuk.roman03@gmail.com>
2022-04-04 00:58:33 +0300

    added test constants

---
+++ src/test/java/com/epam/cargo/service/environment/EnvironmentConstant.java
@@ -0,0 +1,9 @@
+package com.epam.cargo.service.environment;
+
+public interface EnvironmentConstant {
+
+    String DIMENSIONS_FARES_TABLE_CASES = "src/test/resources/static/dimensions_fares_table.csv";
+    String DISTANCE_FARES_TABLE_CASES = "src/test/resources/static/distance_fares_table.csv";
+    String WEIGHT_FARES_TABLE_CASES = "src/test/resources/static/weight_fares_table.csv";
+
+}
